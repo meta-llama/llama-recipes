707af7ea24f20b7b541168ba86c230d6b28886bf
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-07-25 06:26:37 +0000

    adding cuda:0 for non-fsdp situations

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -84,7 +84,7 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                     if train_config.enable_fsdp:
                         batch[key] = batch[key].to(local_rank)
                     else:
-                        batch[key] = batch[key].to('cuda')       
+                        batch[key] = batch[key].to('cuda:0')       
                 outputs = model(**batch)
                 loss = outputs.loss
                 loss = loss / gradient_accumulation_steps
