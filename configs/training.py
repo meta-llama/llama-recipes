48cce515c61a0805f455c349af37b93a0c253306
raghukiran1224 <raghukiran@gmail.com>
2023-07-29 20:22:11 -0400

    Update training.py num steps to report added to config

--- configs/training.py
+++ configs/training.py
@@ -33,6 +33,7 @@ class train_config:
     dist_checkpoint_folder: str="fine-tuned" # will be used if using FSDP
     save_optimizer: bool=False # will be used if using FSDP
     log_file: str="PATH/to/save/logs" #will be used by training script
+    report_steps: int=20 #number of steps to report
 
     
     
