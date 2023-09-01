ccda6fb8ca0fee3fbf7792a6ff1e19d347e01b29
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-01 20:17:39 +0000

    Move inference scripts into example folder

--- src/llama_recipes/inference/__main__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
-import fire
-
-from llama_recipes.inference.inference import main
-
-if __name__ == "__main__":
-    fire.Fire(main)
