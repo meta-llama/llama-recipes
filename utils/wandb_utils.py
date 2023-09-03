70a244f63d34c75c486cd7132a8aeecad48662f3
Maxime <672982+maximegmd@users.noreply.github.com>
2023-09-03 15:38:34 +0200

    use token to login if passed to wandb

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -3,6 +3,8 @@ from configs import wandb_config
 
 def wandb_watch(model, cfg: wandb_config):
     import wandb
+    if wandb_config.wandb_token is not None:
+        wandb.login(key=wandb_config.wandb_token)
     run = wandb.init(
         project=cfg.wandb_project,
         entity=cfg.wandb_entity,
