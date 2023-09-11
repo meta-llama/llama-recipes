647e1e5d75ccc5e0e5c6c08d0f05b185a6864c62
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-11 07:19:10 -0700

    Fix pbar update

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -86,15 +86,15 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                         scaler.step(optimizer)
                         scaler.update()
                         optimizer.zero_grad()
-                        pbar.update(gradient_accumulation_steps)
+                        pbar.update(1)
                 else:
                     # regular backpropagation when fp16 is not used
                     loss.backward()
                     if (step + 1) % gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
                         optimizer.step()
                         optimizer.zero_grad()
-                        pbar.update(gradient_accumulation_steps)
-                
+                        pbar.update(1)
+
                 pbar.set_description(f"Training Epoch: {epoch+1}/{train_config.num_epochs}, step {step}/{len(train_dataloader)} completed (loss: {loss.detach().float()})")
             pbar.close()
                 
