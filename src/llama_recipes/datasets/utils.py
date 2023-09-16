f620f3589d5eca246f6e721c3cd66694f9fc2168
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-09-16 14:14:56 +0000

    Adds length based batch sampler

--- src/llama_recipes/datasets/utils.py
+++ src/llama_recipes/datasets/utils.py
@@ -1,10 +1,13 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
+import random
 from tqdm import tqdm
 from itertools import chain
 
-from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, BatchSampler
+
 
 class Concatenator(object):
     def __init__(self, chunk_size=2048):
@@ -64,3 +67,35 @@ class ConcatDataset(Dataset):
     
     def __len__(self):
         return len(self.samples)
+    
+
+class LengthBasedBatchSampler(BatchSampler):
+    def __init__(self, data_source, batch_size, drop_last, randomize=True):
+        if isinstance(next(iter(data_source)), dict):
+            first_key = next(iter(next(iter(data_source)).keys()))
+            self.lengths = [len(d[first_key]) for d in data_source]
+        else:
+            self.lengths = [len(d) for d in data_source]
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.randomize = randomize
+        
+    def __iter__(self):
+        ids = np.argsort(self.lengths)
+        if self.drop_last:
+            ids = ids[:len(ids) // self.batch_size * self.batch_size]
+            
+        batches = [ids[i:i+self.batch_size] for i in range(0, len(ids), self.batch_size)]
+        
+        if self.randomize:
+            random.shuffle(batches)
+            
+        for b in batches:
+            yield b
+        
+    def __len__(self):
+        if self.drop_last:
+            return len(self.lengths) // self.batch_size 
+        else:
+            return len(self.lengths) // self.batch_size + (len(self.lengths) % self.batch_size > 0)
+        
