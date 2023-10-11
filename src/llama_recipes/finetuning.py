68506ca4d51cf0f7a4b058e52b9dc632c752d230
James Akinniranye <jamesniranye@gmail.com>
2023-10-11 19:40:41 +0100

    temp

--- src/llama_recipes/finetuning.py
+++ src/llama_recipes/finetuning.py
@@ -50,10 +50,7 @@ from dataclasses import asdict
 def main(**kwargs):
     # Update the configuration for the training and sharding process
     update_config((train_config, fsdp_config), **kwargs)
-    print(type(train_config))
-    print(train_config.model_name)
-    print(type(train_config.model_name))
-    print(asdict(train_config))
+    print(asdict(train_config()))
     # Set the seeds for reproducibility
     torch.cuda.manual_seed(train_config.seed)
     torch.manual_seed(train_config.seed)
