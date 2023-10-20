365858219e5fcf90ea0498dc8568d38812e2e143
James Akinniranye <jamesniranye@gmail.com>
2023-10-20 13:21:15 +0100

    moved to v2 dataset

--- examples/incubator_dataset_processor.py
+++ examples/incubator_dataset_processor.py
@@ -5,7 +5,7 @@ B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 def get_custom_dataset(dataset_config, tokenizer, split):
-    dataset = datasets.load_dataset("kiamesdavies/prometheus-grafana-dashboards-full", split="test" if split == "validation" else split)
+    dataset = datasets.load_dataset("kiamesdavies/prometheus-grafana-dashboards-full-v2", split="test" if split == "validation" else split)
 
     prompt = (
         f"{B_INST} {B_SYS}Using the supplied Grafana dashboard graphs/panels in JSON – encompassing title, type, description, and associated metrics – and optionally the header of its associated group and a general dashboard summary, extract insights for each graph in clear, conversational language. These insights can later be transformed into queries. Make sure to point out any distinct groupings, filters, functions, or conditions that might be relevant.{E_SYS}```json\n{{incubate_input}}\n```\n{E_INST}\n```json\n{{incubate_output}}\n```{{eos_token}}"
