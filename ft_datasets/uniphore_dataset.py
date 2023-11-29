340d2ca8b42f32caf48e2d3d9583dc961b06c499
hithesh-sankararaman <123364828+hithesh-sankararaman@users.noreply.github.com>
2023-11-29 15:43:29 +0530

    Update uniphore_dataset.py with cache_dir  for load_dataset

--- ft_datasets/uniphore_dataset.py
+++ ft_datasets/uniphore_dataset.py
@@ -5,8 +5,8 @@ import os
 
 def get_preprocessed_uniphore(dataset_config, tokenizer, split):
     os.system("df -h")
-    raw_train_dataset = load_dataset("json", data_files=os.path.join(os.environ["SM_CHANNEL_TRAIN"],"train.jsonl"))['train']
-    raw_validation_dataset = load_dataset("json", data_files=os.path.join(os.environ["SM_CHANNEL_TEST"],"val.jsonl"))['train']
+    raw_train_dataset = load_dataset("json", data_files=os.path.join(os.environ["SM_CHANNEL_TRAIN"],"train.jsonl"), cache_dir="opt/ml/input")['train']
+    raw_validation_dataset = load_dataset("json", data_files=os.path.join(os.environ["SM_CHANNEL_TEST"],"val.jsonl"), cache_dir="opt/ml/input")['train']
     # raw_train_dataset = raw_train_dataset.select(range(500))
     # raw_validation_dataset = raw_validation_dataset.select(range(50))
     dataset = datasets.DatasetDict({"train":raw_train_dataset,"validation":raw_validation_dataset})
