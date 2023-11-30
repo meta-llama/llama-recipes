d7a5be30592a30311b8bf133126c48aa59067872
hithesh-sankararaman <123364828+hithesh-sankararaman@users.noreply.github.com>
2023-11-30 16:30:29 +0530

    Update mistral_finetuning.py
    at present better transformer doesnt support mistral llm

--- mistral_finetuning.py
+++ mistral_finetuning.py
@@ -105,17 +105,17 @@ def main(**kwargs):
             load_in_8bit=True if train_config.quantization else None,
             device_map="auto" if train_config.quantization else None,
         )
-    if train_config.enable_fsdp and train_config.use_fast_kernels:
-        """
-        For FSDP and FSDP+PEFT, setting 'use_fast_kernels' will enable
-        using of Flash Attention or Xformer memory-efficient kernels 
-        based on the hardware being used. This would speed up fine-tuning.
-        """
-        try:
-            from optimum.bettertransformer import BetterTransformer
-            model = BetterTransformer.transform(model) 
-        except ImportError:
-            print("Module 'optimum' not found. Please install 'optimum' it before proceeding.")
+    # if train_config.enable_fsdp and train_config.use_fast_kernels:
+    #     """
+    #     For FSDP and FSDP+PEFT, setting 'use_fast_kernels' will enable
+    #     using of Flash Attention or Xformer memory-efficient kernels 
+    #     based on the hardware being used. This would speed up fine-tuning.
+    #     """
+    #     try:
+    #         from optimum.bettertransformer import BetterTransformer
+    #         model = BetterTransformer.transform(model) 
+    #     except ImportError:
+    #         print("Module 'optimum' not found. Please install 'optimum' it before proceeding.")
     print_model_size(model, train_config, rank if train_config.enable_fsdp else 0)
 
     # Prepare the model for int8 training if quantization is enabled
