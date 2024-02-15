4913d3ad24db4cf87640d11021c4ff3f6cbb919a
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-02-15 17:40:22 +0000

    Add missing copyright header

--- demo_apps/txt2csv.py
+++ demo_apps/txt2csv.py
@@ -1,3 +1,6 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
 import csv
 
 # Define the input and output file names
