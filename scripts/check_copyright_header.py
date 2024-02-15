46f14e9a333a6e46de7008544ee301e58aaba45b
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-02-15 18:33:21 +0000

    Added blacklist to check_copyright_header.py

--- scripts/check_copyright_header.py
+++ scripts/check_copyright_header.py
@@ -10,9 +10,16 @@ PATTERN = "(Meta Platforms, Inc. and affiliates)|(Facebook, Inc(\.|,)? and its a
 HEADER = """# Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.\n\n"""
 
+#Files in black list must be relative to main repo folder
+BLACKLIST = ["eval/open_llm_leaderboard/hellaswag_utils.py"]
+
 if __name__ == "__main__":
     for ext in ["*.py", "*.sh"]:
         for file in WORK_DIR.rglob(ext):
+            normalized = file.relative_to(WORK_DIR)
+            if normalized.as_posix() in BLACKLIST:
+                continue
+            
             text = file.read_text()
             if not re.search(PATTERN, text):
                 text = HEADER + text
