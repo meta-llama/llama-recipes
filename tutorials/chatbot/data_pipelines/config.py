2580bb14a61a73340481025d2ef2996e20387386
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2024-03-04 16:03:35 +0000

    adding octoAI api key

--- tutorials/chatbot/data_pipelines/config.py
+++ tutorials/chatbot/data_pipelines/config.py
@@ -4,12 +4,11 @@
 import yaml
 import os
 
-
 def load_config():
     # Read the YAML configuration file
     file_path = "./config.yaml"
     with open(file_path, "r") as file:
         config = yaml.safe_load(file)
-
-    config["api_key"] = os.getenv('OPENAI_API_KEY')
+    # Set the API key from the environment variable
+    config["api_key"] = os.environ["OCTOAI_API_TOKEN"]
     return config
