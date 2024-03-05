12bfff4eaa380df5eb375ba00da2f33db0afe2d1
Suraj Subramanian <suraj813@gmail.com>
2024-03-05 15:24:04 -0500

    Add new folder structure, new files and TODOs to port existing content

--- recipes/inference/llama_web_ui/streamlit_llama2.py
+++ recipes/inference/llama_web_ui/streamlit_llama2.py
@@ -1,6 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
+# TODO: Convert this to an ipynb notebook
+
 import streamlit as st
 from langchain.llms import Replicate
 import os
