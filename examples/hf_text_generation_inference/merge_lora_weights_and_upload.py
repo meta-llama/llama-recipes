697a4b7531b20874c846777b95a40abc6cd7cc9f
Alex Li <alex.li@tifin.com>
2024-03-05 10:24:29 +0000

    merging and uploading script added

---
+++ examples/hf_text_generation_inference/merge_lora_weights_and_upload.py
@@ -0,0 +1,50 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+import fire
+import torch
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+def main(push_to_hub: bool = True):
+    base_model = "codellama/CodeLlama-13b-Instruct-hf"
+    peft_model = "/home/ubuntu/llama-recipes-fork/llama-recipes/src/llama_recipes/models/codellama-8bit-json-mkt-research-24-03-04-merged_epoch_8"
+    tokenizer_path = "/home/ubuntu/llama-recipes-fork/llama-recipes/src/llama_recipes/models/codellama-8bit-json-mkt-research-24-03-04-merged_tokenizer"
+    output_dir = "HelixAI/codellama-8bit-json-mkt-research-24-03-04-merged"
+    
+        
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        load_in_8bit=False,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        offload_folder="tmp", 
+    )
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        tokenizer_path
+    )
+        
+    model = PeftModel.from_pretrained(
+        model, 
+        peft_model, 
+        torch_dtype=torch.float16,
+        device_map="auto",
+        offload_folder="tmp",
+    )
+
+    model = model.merge_and_unload()
+    
+    if push_to_hub:
+        print(f"Saving to hub ...")
+        model.push_to_hub(f"{output_dir}", use_temp_dir=True, create_pr=1)
+        tokenizer.push_to_hub(f"{output_dir}", use_temp_dir=True, create_pr=1)
+    else:
+        model.save_pretrained(f"{output_dir}")
+        tokenizer.save_pretrained(f"{output_dir}")
+        print(f"Model saved to {output_dir}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
