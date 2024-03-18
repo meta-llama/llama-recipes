532e88bd41b4e8d551b4d74b80c397f5bda856a4
Allen <39230030+Kyriection@users.noreply.github.com>
2024-03-18 16:39:44 -0500

    Update exp.sh

--- research/long-context-llama/H2O/exp.sh
+++ research/long-context-llama/H2O/exp.sh
@@ -4,10 +4,10 @@ CUDA_VISIBLE_DEVICES=$1 python -u generate.py \
 --model-name meta-llama/Llama-2-7b-hf 
 
 
-CUDA_VISIBLE_DEVICES=$1 python -u generate.py \
---input-path data/summarization/xsum.jsonl \
---output-path xsum_h2o.jsonl \
---model-name meta-llama/Llama-2-7b-hf \
---enable_h2o_generation \
---num_heavy_hitter_tokens 256 \
---num_local_windows 256
+# CUDA_VISIBLE_DEVICES=$1 python -u generate.py \
+# --input-path data/summarization/xsum.jsonl \
+# --output-path xsum_h2o.jsonl \
+# --model-name meta-llama/Llama-2-7b-hf \
+# --enable_h2o_generation \
+# --num_heavy_hitter_tokens 256 \
+# --num_local_windows 256
