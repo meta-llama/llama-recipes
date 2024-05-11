66e1867120cfe91e3c0bb43fff144b1e1dffc4b9
simwiki <23613056+simwiki@users.noreply.github.com>
2024-05-11 11:16:05 +0800

    Fix save metric FileNotFoundError when finetuning

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -103,6 +103,8 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
     val_loss =[]
 
     if train_config.save_metrics:
+        if not os.path.exists(train_config.output_dir):
+            os.makedirs(train_config.output_dir, exist_ok=True)
         metrics_filename = f"{train_config.output_dir}/metrics_data_{local_rank}-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
         train_step_perplexity = []
         train_step_loss = []
