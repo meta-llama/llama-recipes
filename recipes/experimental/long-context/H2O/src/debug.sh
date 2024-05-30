636f874001bd25176036ed7e7315b9239c08172a
Allen <39230030+Kyriection@users.noreply.github.com>
2024-05-29 23:16:47 -0700

    Create debug.sh

---
+++ recipes/experimental/long-context/H2O/src/debug.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1 python -u run_needle_haystack_test.py \
+--input-path data/needle_test/Huggingface \
+--output-path needle_test_results/huggingface/llama-3-8b-instruct/ \
+--model-name meta-llama/Meta-Llama-3-8B-Instruct 
