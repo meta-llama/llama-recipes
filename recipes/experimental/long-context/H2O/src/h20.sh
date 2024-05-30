7084c1b474828e4383b8b1c116c86ba2fbd71395
Allen <39230030+Kyriection@users.noreply.github.com>
2024-05-30 00:02:49 -0700

    upload

--- recipes/experimental/long-context/H2O/src/h20.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python -u run_needle_haystack_test.py \
---input-path data/needle_test/Huggingface \
---output-path needle_test_results/huggingface/llama-3-8b-instruct-h2o-4096/ \
---model-name meta-llama/Meta-Llama-3-8B-Instruct \
---enable_h2o_generation \
---num_window_length 4096 \
---num_heavy_hitter_tokens 2048
