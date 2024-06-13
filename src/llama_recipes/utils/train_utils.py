e33a23d02483fbdc5e2ffadd3775a2c54f64f543
shiyan <yan.shi@intel.com>
2024-06-13 00:50:36 -0700

    Add exit message to indicate amp.GradScaler() is not supported on XPU.

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -93,6 +93,8 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
     if train_config.use_fp16 and train_config.enable_fsdp:
         scaler = ShardedGradScaler()
     elif train_config.use_fp16 and not train_config.enable_fsdp:
+        if is_xpu_available:
+          raise NotImplementedError("amp.GradScaler() is not supported on XPU backend")
         scaler = torch.cuda.amp.GradScaler()
     if train_config.enable_fsdp:
         world_size = int(os.environ["WORLD_SIZE"])
