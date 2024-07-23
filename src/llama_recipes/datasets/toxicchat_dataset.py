29d01b0798f28aea26d11304aaf9654478ce7d82
Thomas Robinson <trobinson@meta.com>
2024-07-23 15:59:34 +0000

    Address feedback not possible before launch

--- src/llama_recipes/datasets/toxicchat_dataset.py
+++ src/llama_recipes/datasets/toxicchat_dataset.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+# This software may be used and distributed according to the terms of the Llama 3.1 Community License Agreement.
 
-# For dataset details visit: https://huggingface.co/datasets/samsum
+# For dataset details visit: https://huggingface.co/datasets/lmsys/toxic-chat
 
 import copy
 import datasets
