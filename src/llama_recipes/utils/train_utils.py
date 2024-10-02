a5704bf11c9b403f9f18571836a1b0b9319b9c7a
Matthias Reso <13337103+mreso@users.noreply.github.com>
2024-10-02 15:28:31 -0700

    Apply suggestions from code review
    Fix div through zero

--- src/llama_recipes/utils/train_utils.py
+++ src/llama_recipes/utils/train_utils.py
@@ -228,8 +228,8 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                 val_step_perplexity.extend(temp_step_perplexity)
             should_save_model = train_config.save_model and eval_epoch_loss < best_val_loss
         
+        checkpoint_start_time = time.perf_counter()
         if should_save_model:
-            checkpoint_start_time = time.perf_counter()
             if train_config.enable_fsdp:
                 dist.barrier()
             if train_config.use_peft:
@@ -277,19 +277,19 @@ def train(model, train_dataloader,eval_dataloader, tokenizer, optimizer, lr_sche
                     
             if train_config.enable_fsdp:
                 dist.barrier()
-            checkpoint_end_time = time.perf_counter() - checkpoint_start_time
-            checkpoint_times.append(checkpoint_end_time)
+        checkpoint_end_time = time.perf_counter() - checkpoint_start_time
+        checkpoint_times.append(checkpoint_end_time)
 
-            if train_config.run_validation:
-                if eval_epoch_loss < best_val_loss:
-                    best_val_loss = eval_epoch_loss
-                    if train_config.enable_fsdp:
-                        if rank==0:
-                            print(f"best eval loss on epoch {epoch+1} is {best_val_loss}")
-                    else:
+        if train_config.run_validation:
+            if eval_epoch_loss < best_val_loss:
+                best_val_loss = eval_epoch_loss
+                if train_config.enable_fsdp:
+                    if rank==0:
                         print(f"best eval loss on epoch {epoch+1} is {best_val_loss}")
-                val_loss.append(float(best_val_loss))
-                val_prep.append(float(eval_ppl))
+                else:
+                        print(f"best eval loss on epoch {epoch+1} is {best_val_loss}")
+            val_loss.append(float(best_val_loss))
+            val_prep.append(float(eval_ppl))
         if train_config.enable_fsdp:
             if rank==0:
                 print(f"Epoch {epoch+1}: train_perplexity={train_perplexity:.4f}, train_epoch_loss={train_epoch_loss:.4f}, epoch time {epoch_end_time}s")
