896b9ee2d67a4e4d62c096d731c6ae8cc87d38c3
Sanyam Bhutani <sanyambhutani@meta.com>
2025-01-21 13:58:26 -0800

    update imports

--- src/llama_cookbook/datasets/__init__.py
+++ src/llama_cookbook/datasets/__init__.py
@@ -3,11 +3,11 @@
 
 from functools import partial
 
-from llama_recipes.datasets.grammar_dataset.grammar_dataset import get_dataset as get_grammar_dataset
-from llama_recipes.datasets.alpaca_dataset import InstructionDataset as get_alpaca_dataset
-from llama_recipes.datasets.custom_dataset import get_custom_dataset,get_data_collator
-from llama_recipes.datasets.samsum_dataset import get_preprocessed_samsum as get_samsum_dataset
-from llama_recipes.datasets.toxicchat_dataset import get_llamaguard_toxicchat_dataset as get_llamaguard_toxicchat_dataset
+from llama_cookbook.datasets.grammar_dataset.grammar_dataset import get_dataset as get_grammar_dataset
+from llama_cookbook.datasets.alpaca_dataset import InstructionDataset as get_alpaca_dataset
+from llama_cookbook.datasets.custom_dataset import get_custom_dataset,get_data_collator
+from llama_cookbook.datasets.samsum_dataset import get_preprocessed_samsum as get_samsum_dataset
+from llama_cookbook.datasets.toxicchat_dataset import get_llamaguard_toxicchat_dataset as get_llamaguard_toxicchat_dataset
 DATASET_PREPROC = {
     "alpaca_dataset": partial(get_alpaca_dataset),
     "grammar_dataset": get_grammar_dataset,
