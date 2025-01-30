479b1fbbd76e3eed0c62f201f90f615748eeb01e
Justin Lee <justinai@fb.com>
2025-01-29 23:23:43 -0800

    updated mmlu meta-eval for prompt migration

--- end-to-end-use-cases/benchmarks/llm_eval_harness/meta_eval/work_dir/mmlu/utils.py
+++ end-to-end-use-cases/benchmarks/llm_eval_harness/meta_eval/work_dir/mmlu/utils.py
@@ -1,10 +1,22 @@
 import string
+
 import datasets
 
+
 def doc_to_text(doc: dict) -> str:
-    # Strip out the last two characters, which is a space and the answer
-    # E.g., "Answer: B" -> "Answer:"
-    return doc["input_final_prompts"][0][:-2]
+    question, choice = doc["input_question"], str(doc["input_choice_list"])
+    prompt = [
+        "You are a knowledgeable and insightful assistant designed to provide guidance on multiple-choice questions. Your role involves analyzing questions, assessing answer choices, and offering well-reasoned explanations to enhance understanding. By breaking down complex concepts, you help users develop critical thinking skills and improve their decision-making process. You strive to present information in a clear, structured manner while adapting to the user's level of expertise. Ultimately, your goal is to foster deeper comprehension and confidence in tackling multiple-choice assessments.",
+    ]
+    template = f"<|start_header_id|>user<|end_header_id|>{prompt[0]}. Question: {question}\n {choice}\n<|eot_id|> \n\n<|start_header_id|>assistant<|end_header_id|>"
+    return template
+
+
+# def doc_to_text(doc: dict) -> str:
+#     # Strip out the last two characters, which is a space and the answer
+#     # E.g., "Answer: B" -> "Answer:"
+#     return doc["input_final_prompts"][0][:-2]
+
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc: dict) -> dict:
@@ -20,12 +32,22 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
         }
         return out_doc
 
+    dataset = dataset.select(range(1500, len(dataset)))
+
     dataset = dataset.select_columns(
-        ["input_question", "input_correct_responses", "input_final_prompts", "is_correct", "input_question_hash",
-         "input_choice_list"])
+        [
+            "input_question",
+            "input_correct_responses",
+            "input_final_prompts",
+            "is_correct",
+            "input_question_hash",
+            "input_choice_list",
+        ]
+    )
     dataset = dataset.rename_column("is_correct", "previously_is_correct")
     dataset = dataset.map(_process_doc)
     return dataset.map(_process_doc)
+
 
 def doc_to_target(doc: dict) -> str:
     return doc["gold"]
