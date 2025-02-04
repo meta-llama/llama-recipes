73e8da51768cbe7f9aebbd2a47c211b92e1aed26
Igor Kasianenko <igorka@meta.com>
2025-02-04 16:14:54 +0000

    deprecate octoai

--- getting-started/inference/README.md
+++ getting-started/inference/README.md
@@ -3,4 +3,3 @@
 This folder contains scripts to get you started with inference on Meta Llama models.
 
 * [Local Inference](./local_inference/) contains scripts to do memory efficient inference on servers and local machines
-* [Mobile Inference](./mobile_inference/) has scripts using MLC to serve Llama on Android (h/t to OctoAI for the contribution!)
--- src/llama_cookbook/inference/llm.py
+++ src/llama_cookbook/inference/llm.py
@@ -157,37 +157,3 @@ class ANYSCALE(LLM):
             "mistralai/Mistral-7B-Instruct-v0.1",
             "HuggingFaceH4/zephyr-7b-beta",
         ]
-
-class OctoAI(LLM):
-    """Accessing OctoAI"""
-
-    def __init__(self, model: str, api_key: str) -> None:
-        super().__init__(model, api_key)
-        self.client = openai.OpenAI(base_url="https://text.octoai.run/v1", api_key=api_key)  # noqa
-
-    @override
-    def query(self, prompt: str) -> str:
-        # Best-level effort to suppress openai log-spew.
-        # Likely not work well in multi-threaded environment.
-        level = logging.getLogger().level
-        logging.getLogger().setLevel(logging.WARNING)
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant. Keep your responses limited to one short paragraph if possible."},
-                {"role": "user", "content": prompt},
-            ],
-            max_tokens=MAX_TOKENS,
-            temperature=TEMPERATURE,
-            top_p=TOP_P,
-        )
-        logging.getLogger().setLevel(level)
-        return response.choices[0].message.content
-
-    @override
-    def valid_models(self) -> list[str]:
-        return [
-            "llamaguard-2-8b",
-            "meta-llama-3-8b-instruct",
-            "meta-llama-3-70b-instruct",        
-        ]
